4d382f78d1b31b9ce42d4ccdb5f4c8cacb12e537
Thiago Santos <tos.oliveira@gmail.com>
2023-08-02 13:35:30 -0300

    feat: updating README and creating subContext feature

--- README.md
+++ README.md
@@ -6,13 +6,34 @@
 [![Packages](https://david-dm.org/maxmilhas/winston-context-logger-async-local-storage.svg)](https://david-dm.org/maxmilhas/winston-context-logger-async-local-storage)
 [![npm version](https://badge.fury.io/js/%40maxmilhas%2Fwinston-context-logger-async-local-storage.svg)](https://badge.fury.io/js/%40maxmilhas%2Fwinston-context-logger-async-local-storage)
 
-Never forget to write your readme! Also, don't forget to add codeclimate key to your github environment variables (as **CC_TEST_REPORTER_ID**) and to update the url here with the one provided by them!
+Context provider for winston-context-logger implemented by using a elastic-apm-nodejs transaction.
+With this context provider, it is possible to have a fully contextualized logger where all the context is tied to apm transactions, and the correlationid generated will be the traceparent, contributing even to a trace distribution over a micro service scenario done completely by the apm api.
 
 ## How to Install
 
 ```
-npm i winston-context-logger-async-local-storage
+npm i winston-context-logger-apm-storage
 ```
+
+## How to use it
+
+Just pass it to ContextLogger during instantiation and you're done.
+
+```ts
+new ContextLogger(logger, apmContextProvider);
+```
+
+You can also create sub custom contexts that will inherit all the metadata from the parents:
+
+```ts
+await apmContextProvider.subContext('my sub routine', async () => {
+  // Do something here
+  logger.addMeta('newOne', 'new metadata');
+  logger.info('Log with all the inherited metadata and the newOne');
+});
+  logger.info('Log without the newOne metadata');
+```
+
 
 ## License
 
--- src/request-context.ts
+++ src/request-context.ts
@@ -1,13 +1,16 @@
 import { ContextInfoProvider } from 'winston-context-logger';
 import apm from 'elastic-apm-node';
+import { AsyncLocalStorage } from 'async_hooks';
 
 const loggerContextSymbol = Symbol('LoggerContext');
 const onContextEndSymbol = Symbol('OnContextEnd');
+const subStorageSymbol = Symbol('SubStorage');
 const flushSymbol = Symbol('Flush');
+
 export class RequestContext {
-	readonly privateMeta: {
-		[key: symbol]: object;
-	} = {};
+	readonly privateMeta = new Map<symbol, unknown>();
+
+	[subStorageSymbol]: AsyncLocalStorage<RequestContext> | undefined;
 
 	constructor(
 		public readonly correlationId: string,
@@ -21,7 +24,7 @@ function monkeyPatchOnEnd(
 ) {
 	const end = transaction.end.bind(transaction);
 	let list = [] as Array<(routine?: string) => void>;
-	context.privateMeta[onContextEndSymbol] = list;
+	context.privateMeta.set(onContextEndSymbol, list);
 	function flush() {
 		const current = list;
 		list = [];
@@ -35,11 +38,24 @@ function monkeyPatchOnEnd(
 			}
 		});
 	}
-	context.privateMeta[flushSymbol] = flush;
+	context.privateMeta.set(flushSymbol, flush);
 	transaction.end = (...args: Parameters<typeof transaction.end>) => {
 		end(...args);
 		flush();
 	};
+}
+
+function createSubContext(
+	current: RequestContext,
+	correlationId: string,
+	subRoutine: string,
+) {
+	const context = new RequestContext(correlationId, subRoutine);
+	const { privateMeta } = current;
+	for (const [key, value] of privateMeta) {
+		context.privateMeta.set(key, value);
+	}
+	return context;
 }
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -49,54 +65,101 @@ const rootContext = new RequestContext('root', 'root');
 export class ApmContextProvider<T extends object>
 	implements ContextInfoProvider<T>
 {
+	/**
+	 * @returns the current RequestContext object
+	 */
 	currentContext() {
 		const transaction = apm.currentTransaction;
+		let context: RequestContext | undefined;
 		if (!transaction) {
-			return rootContext;
-		}
-
-		let context = (transaction as unknown as Record<symbol, RequestContext>)[
-			loggerContextSymbol
-		];
-		if (!context) {
-			context = new RequestContext(
-				transaction.traceparent ?? transaction.ids['trace.id'],
-				transaction.name,
-			);
-			(transaction as unknown as Record<symbol, RequestContext>)[
+			context = rootContext;
+		} else {
+			context = (transaction as unknown as Record<symbol, RequestContext>)[
 				loggerContextSymbol
-			] = context;
-			monkeyPatchOnEnd(transaction, context);
+			];
+			if (!context) {
+				context = createSubContext(
+					rootContext,
+					transaction.traceparent ?? transaction.ids['trace.id'],
+					transaction.name,
+				);
+				(transaction as unknown as Record<symbol, RequestContext>)[
+					loggerContextSymbol
+				] = context;
+				monkeyPatchOnEnd(transaction, context);
+			}
+		}
+		const subStorage = context[subStorageSymbol];
+		if (subStorage) {
+			const subContext = subStorage.getStore();
+			if (subContext) {
+				context = subContext;
+			}
 		}
 
 		return context;
 	}
 
+	/**
+	 * The current correlation Id
+	 */
 	get correlationId() {
 		return this.currentContext().correlationId;
 	}
 
+	/**
+	 * The current routine
+	 */
 	get routine() {
 		return this.currentContext().routine;
 	}
 
+	/**
+	 * @returns Returns the logger metadata
+	 */
 	getContextInfo() {
-		return this.currentContext().privateMeta[loggerContextSymbol];
+		return this.currentContext().privateMeta.get(
+			loggerContextSymbol,
+		) as Partial<T>;
 	}
+	/**
+	 * Sets te current logger metadata;
+	 * @param value the logger metadata value
+	 */
 	setContextInfo(value: object) {
-		this.currentContext().privateMeta[loggerContextSymbol] = value;
+		const context = this.currentContext();
+		context.privateMeta.set(loggerContextSymbol, value);
+	}
+
+	/**
+	 * creates a new sub contexts that will inherits every parent metadata
+	 * @param subRoutine
+	 */
+	async subContext(subRoutine: string, callback: () => unknown) {
+		const current = this.currentContext();
+		let storage = current[subStorageSymbol];
+		if (!storage) {
+			storage = new AsyncLocalStorage();
+			current[subStorageSymbol] = storage;
+		}
+		const context = createSubContext(
+			current,
+			current.correlationId,
+			subRoutine,
+		);
+		await storage.run(context, callback);
 	}
 
 	onContextEnd(callback: () => void): void {
 		(
-			this.currentContext().privateMeta[onContextEndSymbol] as
+			this.currentContext().privateMeta.get(onContextEndSymbol) as
 				| Array<() => void>
 				| undefined
 		)?.push(callback);
 	}
 
 	flush() {
-		(this.currentContext().privateMeta[flushSymbol] as () => void)();
+		(this.currentContext().privateMeta.get(flushSymbol) as () => void)();
 	}
 }
 
