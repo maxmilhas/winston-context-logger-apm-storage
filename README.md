2fcae7ac7ed4d70fb49b493db4657f25b7ee43bb
Thiago Santos <tos.oliveira@gmail.com>
2023-08-02 07:24:13 -0300

    feat: first version

---
+++ README.md
@@ -0,0 +1,19 @@
+[![Actions Status](https://github.com/maxmilhas/winston-context-logger-async-local-storage/workflows/build/badge.svg)](https://github.com/maxmilhas/winston-context-logger-async-local-storage/actions)
+[![Actions Status](https://github.com/maxmilhas/winston-context-logger-async-local-storage/workflows/test/badge.svg)](https://github.com/maxmilhas/winston-context-logger-async-local-storage/actions)
+[![Actions Status](https://github.com/maxmilhas/winston-context-logger-async-local-storage/workflows/lint/badge.svg)](https://github.com/maxmilhas/winston-context-logger-async-local-storage/actions)
+[![Test Coverage](https://api.codeclimate.com/v1/badges/65e41e3018643f28168e/test_coverage)](https://codeclimate.com/github/maxmilhas/winston-context-logger-async-local-storage/test_coverage)
+[![Maintainability](https://api.codeclimate.com/v1/badges/65e41e3018643f28168e/maintainability)](https://codeclimate.com/github/maxmilhas/winston-context-logger-async-local-storage/maintainability)
+[![Packages](https://david-dm.org/maxmilhas/winston-context-logger-async-local-storage.svg)](https://david-dm.org/maxmilhas/winston-context-logger-async-local-storage)
+[![npm version](https://badge.fury.io/js/%40maxmilhas%2Fwinston-context-logger-async-local-storage.svg)](https://badge.fury.io/js/%40maxmilhas%2Fwinston-context-logger-async-local-storage)
+
+Never forget to write your readme! Also, don't forget to add codeclimate key to your github environment variables (as **CC_TEST_REPORTER_ID**) and to update the url here with the one provided by them!
+
+## How to Install
+
+```
+npm i winston-context-logger-async-local-storage
+```
+
+## License
+
+Licensed under [MIT](https://en.wikipedia.org/wiki/MIT_License).
---
+++ src/request-context.ts
@@ -0,0 +1,101 @@
+import { ContextInfoProvider } from 'winston-context-logger';
+import apm from 'elastic-apm-node';
+
+const loggerContextSymbol = Symbol('LoggerContext');
+const onContextEndSymbol = Symbol('OnContextEnd');
+const flushSymbol = Symbol('Flush');
+export class RequestContext {
+	readonly privateMeta: {
+		[key: symbol]: object;
+	} = {};
+
+	constructor(
+		public readonly correlationId: string,
+		public readonly routine: string,
+	) {}
+}
+
+function monkeyPatchOnEnd(
+	transaction: apm.Transaction,
+	context: RequestContext,
+) {
+	const end = transaction.end.bind(transaction);
+	let list = [] as Array<(routine?: string) => void>;
+	context.privateMeta[onContextEndSymbol] = list;
+	function flush() {
+		const current = list;
+		list = [];
+		current.forEach((callback) => {
+			try {
+				callback(transaction.name);
+			} catch (error) {
+				console.error(
+					`Error when calling context end callback ${(error as Error).stack}`,
+				);
+			}
+		});
+	}
+	context.privateMeta[flushSymbol] = flush;
+	transaction.end = (...args: Parameters<typeof transaction.end>) => {
+		end(...args);
+		flush();
+	};
+}
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export type Func = (...args: any[]) => any;
+const rootContext = new RequestContext('root', 'root');
+
+export class ApmContextProvider<T extends object>
+	implements ContextInfoProvider<T>
+{
+	currentContext() {
+		const transaction = apm.currentTransaction;
+		if (!transaction) {
+			return rootContext;
+		}
+
+		let context = (transaction as unknown as Record<symbol, RequestContext>)[
+			loggerContextSymbol
+		];
+		if (!context) {
+			context = new RequestContext(
+				transaction.traceparent ?? transaction.ids['trace.id'],
+				transaction.name,
+			);
+			(transaction as unknown as Record<symbol, RequestContext>)[
+				loggerContextSymbol
+			] = context;
+			monkeyPatchOnEnd(transaction, context);
+		}
+
+		return context;
+	}
+
+	get correlationId() {
+		return this.currentContext().correlationId;
+	}
+
+	get routine() {
+		return this.currentContext().routine;
+	}
+
+	getContextInfo() {
+		return this.currentContext().privateMeta[loggerContextSymbol];
+	}
+	setContextInfo(value: object) {
+		this.currentContext().privateMeta[loggerContextSymbol] = value;
+	}
+
+	onContextEnd(callback: () => void): void {
+		(
+			this.currentContext().privateMeta[onContextEndSymbol] as Array<() => void>
+		).push(callback);
+	}
+
+	flush() {
+		(this.currentContext().privateMeta[flushSymbol] as () => void)();
+	}
+}
+
+export const apmContextProvider = new ApmContextProvider();
